a37f44c8f49d9e324cfe685f0381acaf32aab3e7
Nathan Keith <nashvegasnate@gmail.com>
2021-04-21 14:25:34 -0500

    added partial joke functionality

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,66 @@
 import React, { useState } from 'react';
+import {
+  Button,
+  Popover,
+  PopoverHeader,
+  PopoverBody
+} from 'reactstrap';
 import './App.scss';
+import getJoke from '../helpers/data/jokeData';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [allJokes, setAllJokes] = useState([]);
+  const [singleJoke, setSingleJoke] = useState({});
+  const [showPunchline, setShowPunchline] = useState(false);
+  const [popoverOpen, setPopoverOpen] = useState(false);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+  const handleClick = () => {
+    if (showPunchline) {
+      getJoke()
+        .then((jokes) => {
+          setAllJokes(jokes);
+          setSingleJoke(jokes);
+        });
+      setShowPunchline(false);
+      setSingleJoke(allJokes);
+    } else {
+      setShowPunchline(true);
+    }
   };
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
+      {popoverOpen ? ''
+        : <Button color="info" onClick={handleClick}>
+        {showPunchline ? 'Get Another Joke' : 'Get a Joke' }
+      </Button>
+      }
+      <h1>{singleJoke.joke}</h1>
+      <p>{showPunchline && singleJoke.punchline}</p>
+      {showPunchline ? ''
+        : <div>
+      <Button id="Popover1" type="button">
+        Get Punchline
+      </Button>
+      <Popover
+        placement="bottom"
+        isOpen={popoverOpen}
+        target="Popover1"
+        toggle={toggle}
+        trigger="hover"
         >
-          I am THIS button
-        </button>
+        <PopoverHeader>Possible Answers</PopoverHeader>
+        <PopoverBody>
+          <ul>
+            {singleJoke.punchlines?.map((pa, i) => <li key={i}>{pa}</li>)}
+        </ul>
+        </PopoverBody>
+      </Popover>
       </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      }
     </div>
   );
 }
